8b51a5384af4f59e4248b7e488eb0b31188dff95
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2021-11-01 14:59:22 +0100

    Update Tweet.py

--- Core/Tweet.py
+++ Core/Tweet.py
@@ -3,17 +3,56 @@ import datetime
 def gethashtagsfromcontent(content):
     return []
 
-class GeoData:
+class GeoPoint:
     def __init__(self, x, y):
         self.x = x
         self.y = x
 
+class entities:
+    def __init__(self, h, um, url):
+        self.hashtags
+        self.user_mentions
+        self.urls
+
+class Place:
+    def __init__(self, h, um, url):
+        self.place_type
+        self.country
+        self.ccode
+        self.full_name
+        self.name
+        self.id
+
+#this class is just a databox and multiple instances of the same user can exist
+class User:
+    def __init__(self):
+        self.time_zone
+        self.created_at
+        self.friends_count
+        self.followers_count
+        self.location
+        self.lang
+        self.statuses_count
+        self.verified
+        self.name
+        self.id
+        self.favourites_count
+        self.screenname #unique
+
+
 class Tweet:
     def __init__(self, author, date, id, content, author_id):
+        self.id
         self.text
         self.creation_date
         self.geo
-
+        self.retweet_count
+        self.in_reply_to_status_id_str
+        self.in_reply_to_status_id
+        self.in_reply_to_user_id_str
+        self.coordinates
+        self.place
+        self.user
 
 
 
@@ -27,3 +66,4 @@ class Tweet:
 
 
 def gettweetfromjson(jsoncontent):
+    pass
