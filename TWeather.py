61d0e21ce454637a429627a01eaf86045c6dcc7a
gonz-tyler <gonzaloescolartyler@gmail.com>
2021-11-03 17:06:36 +0100

    updated buttons

--- TWeather.py
+++ TWeather.py
@@ -1,5 +1,6 @@
 from eca import *
 import eca.http
+import random
 
 from eca.generators import start_offline_tweets
 import datetime
@@ -7,6 +8,7 @@ import textwrap
 from WeatherEventGen import *
 import Classifier
 import WeatherCondition
+
 
 #Adding a handler for the search button press.
 def add_request_handlers(httpd):
@@ -16,6 +18,7 @@ def add_request_handlers(httpd):
 def setup(ctx, e):
    start_tweets(Classifier.OfficialTweets, time_factor=10000, event_name='chirpofficial')
    start_tweets(Classifier.RegularTweets, time_factor=10000, event_name='chirpregular')
+   
    
    #tweetonce(Classifier.OfficialTweets[0])
 
@@ -32,7 +35,17 @@ def tweet(ctx, e):
    # generate output
    #print(tweet['user']['name'])
    emit('official', tweet)
-   emit("weather", tweet)
+   weatherCond = (WeatherCondition.Extract(tweet))
+   print(weatherCond['Temperature'])
+   print(weatherCond['Time'])
+
+   emit('updateGraph',{
+    'action': 'add',
+    'value': {
+       'Time':1,#weatherCond['Time'],
+       'Temp':weatherCond['Temperature']}
+   })
+
 
 @event('chirpregular')
 def tweet(ctx, e):
@@ -60,15 +73,11 @@ def tweet(ctx, e):
 
 #for making search functionality and graph work, in progress
 @event('search')
-def updategraph(context,e): #WHEN USER WANTS TO SEARCH, DECODE MSG
+def search(context,e): #WHEN USER WANTS TO SEARCH, DECODE MSG
    print("Testing, is the search button working???")
+
+# define a normal Python function
+def clip(lower, value, upper):
+    return max(lower, min(value, upper))
+
    
-   #get location from search thing
-   
-   #get all the weather info from that location
-   sample = e.data
-   #emit into graph:
-   emit('updateGraph',{
-    'action': 'add',
-    'value': sample
-   })
