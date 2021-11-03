5e9e86d4ffc7bc319785b6a189dabbd36424d948
D Osinga <douwe@oscom.nl>
2021-11-03 17:07:33 +0100

    removed debug stuff

--- TWeather.py
+++ TWeather.py
@@ -30,8 +30,6 @@ def tweet(ctx, e):
    tweet['user']['profile_image_url_https'] = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'   
 
    # generate output
-   print(tweet['user']['name'])
-   print(tweet['text'])
    emit('official', tweet)
    emit("weather", tweet)
 
@@ -40,21 +38,9 @@ def tweet(ctx, e):
    # we receive a tweet
    tweet = e.data
 
-   #I've been able to locate the user image urls, because I couldn't stand the 'image not found' icon on every tweet. -Douwe Osinga
-   #class of tweet is dict, so try to change the value of the image keys to the default twitter user image.
    tweet['user']['profile_image_url'] = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'
    tweet['user']['profile_image_url_https'] = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'   
 
-   # parse date (NOT USED RIGHT NOW)
-   #time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
-   
-   # nicify text (NOT USED RIGHT NOW)
-   #text = textwrap.fill(tweet['text'],initial_indent='    ', subsequent_indent='    ')
-   
-   #update the weather graph based on location data.
-   
-   # generate output
-   #print(tweet['user']['name'])
    emit('regular', tweet)
 
 
