c9bae7fb108fd296eca54133a8f245cfce3cbcbe
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2021-11-04 16:50:14 +0100

    af

--- WeatherEventGen.py
+++ WeatherEventGen.py
@@ -1,5 +1,6 @@
 from os import times
-import threading
+from threading import Thread
+from TwitterAPIHandler import *
 import time
 from datetime import datetime
 from eca import fire_global
@@ -7,8 +8,89 @@ from eca.generators import *
 import json
 import ctypes
 import TWeather
-import Classifier
+from Classifier import *
+from Search import *
 from eca import fire, get_context, context_switch, register_auxiliary, auxiliary
+
+def processTweet(data, includes):
+    try:
+      tweetdata = data
+      tweetincl = includes
+      #print(tweetdata['text'])
+   
+    except:
+      print("something went wrong here")
+      return
+      #parse data into accepted format
+      #data needed:
+      #   tweet.user.screen_name
+      #   tweet.user.name
+      #   tweet.user.profile_image_url
+      #   tweet.created_at
+      #   tweet.text
+      #   tweet.entities
+    twet = {}
+    twet['user'] = {}
+    twet['user']['screen_name'] = tweetincl['users'][0]['username']
+    twet['user']['name'] = tweetincl['users'][0]['name']
+    twet['user']['profile_image_url'] = tweetincl['users'][0]['profile_image_url']
+    twet['created_at'] = datetime.strptime(tweetdata['created_at'], "%Y-%m-%dT%H:%M:%S.000Z").strftime('%a %b %d %H:%M:%S %z %Y')
+    twet['text'] = tweetdata['text']
+    twet['entities'] = {}
+    if "entities" in tweetdata:
+      twet['entities']['hashtags'] = []
+      twet['entities']['user_mentions'] = []
+      twet['entities']['urls'] = []
+
+      if "hashtags" in tweetdata['entities']:
+         for item in tweetdata['entities']['hashtags']:
+            twet['entities']['hashtags'].append({"text":item['tag'], "indices":[item['start'], item['end']]})
+
+      if "mentions" in tweetdata['entities']:
+         for item in tweetdata['entities']['mentions']:
+            twet['entities']['user_mentions'].append({"id_str":item["id"],"id":int(item["id"]),"screen_name":item['username'],"name":item['username'], "indices":[item['start'], item['end']]})
+      
+      if "urls" in tweetdata['entities']:
+         for item in tweetdata['entities']['urls']:
+            twet['entities']['urls'].append({"url":item['url'],"display_url":item['display_url'], "indices":[item['start'], item['end']]})
+
+    
+    if not checksearch(twet):
+        return
+
+    if Classifier.isofficial(twet):
+        fire_global('chirpofficial', twet)
+    else:
+        #if Classifier.tweetIsAboutWeather_Certainty(twet) > Classifier.classifier_threshold:
+        fire_global("chirpregular", twet)
+       
+
+def onReceiveTweet(json_file):
+   #if Classifier.tweetIsAboutWeather_Certainty(json_file) < classifier_threshold:
+   #   return
+   id = json_file['data']['id']
+   url = "https://api.twitter.com/2/tweets/{}?{}&{}&{}&{}".format(id, expansions, tweet_fields, user_fields, place_fields)
+
+
+   resp = requests.request("GET", url, auth=bearer_oauth)
+
+   if resp.status_code != 200:
+      raise Exception(
+         "Request returned an error: {} {}".format(
+            resp.status_code, resp.text
+            )
+        )
+   try:
+      tweetdata = json.loads(resp.content)
+      tweetincl = tweetdata['includes']
+      tweetdata = tweetdata['data']
+      processTweet(tweetdata, tweetincl)
+   except:
+      print(resp.content)
+
+
+
+
 
 def supertweetgen(datacpy, timescale=1000, loop=True, begintime="Mon Nov 28 15:55:54 +0000 2011", classifier_threshold = 2):
     #our own event generator, 
@@ -57,5 +139,8 @@ def supertweetgen(datacpy, timescale=1000, loop=True, begintime="Mon Nov 28 15:5
         regularcatchuptweets = []
     if (loop):
         supertweetgen(datacpy, timescale=1000, loop=True, begintime=begintime, classifier_threshold = 2)
+    else:
+        twitterthread = Thread(target = get_stream, args =(set, onReceiveTweet, ))
+        twitterthread.start()
         #repeat it
 
