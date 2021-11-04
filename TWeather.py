012483ed4141647942b7be59ce344178f492c3c3
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2021-11-04 16:21:25 +0100

    documented everything

--- TWeather.py
+++ TWeather.py
@@ -25,16 +25,17 @@ def add_request_handlers(httpd):
     httpd.add_route('/api/search', http.GenerateEvent('Search'), methods=['POST'])
     httpd.add_route('/api/cache', http.GenerateEvent('rqcache'), methods=['POST'])
 
-
+#add event for search
 @event("Search")
 def rqSearch(ctx, e):
+   
    print(e.data)
    global searchval 
    searchval = e.data['search2'] #I don't know why they called it search2
 
    emit('official', {}) #signal to clear the twitter feed
-   emit('regular', {})
-
+   emit('regular', {}) #ive made it so that the twitter feed clears when an empty object is sent
+   #also request the cache
    fire("rqcache")
 
 @event("rqcache")
@@ -52,6 +53,7 @@ def rqCache(ctx, e):
 
          
    global GraphMemory
+   #only push the last 50 tweets to the weathergraph
    for tweet in newlist[-GraphMemory:]  if len(newlist) > GraphMemory else newlist:
       weatherCond = WeatherCondition.ExtractWeatherFromTweet(tweet)
       global BaseTime
@@ -74,19 +76,16 @@ def rqCache(ctx, e):
 
 @event('init')
 def setup(ctx, e):
-   #start_tweets(Classifier.OfficialTweets, time_factor=10000, event_name='chirpofficial')
+   #i've craeted my own event generator, this allows us to have more control over the generation process
    thread = Thread(target = supertweetgen, args = (Classifier.data, 10000, False, "Mon Nov 20 15:55:54 +0000 2011"))
    thread.start()
-   #start_tweets(Classifier.RegularTweets, time_factor=10000, event_name='chirpregular')
-   
-   
-   #tweetonce(Classifier.OfficialTweets[0])
 
+   
 BaseTime = datetime.strptime("Mon oct 10 00:00:00 2011", '%a %b %d %H:%M:%S %Y')
 
 @event('chirpofficial')
 def tweet(ctx, e):
-   # we receive a tweet
+       # we receive a tweet
    tweetls = e.data
 
    #I've been able to locate the user image urls, because I couldn't stand the 'image not found' icon on every tweet. -Douwe Osinga
@@ -94,6 +93,7 @@ def tweet(ctx, e):
    if not isinstance(tweetls, list):
       tweetls = [tweetls]
 
+   #do the same thing with the weathergraph as in rqcache()
    global GraphMemory
    for tweet in tweetls[-GraphMemory:]  if len(tweetls) > GraphMemory else tweetls:
       weatherCond = WeatherCondition.ExtractWeatherFromTweet(tweet)
@@ -106,12 +106,14 @@ def tweet(ctx, e):
       emit('updateWeatherStats',weatherCond)
 
    for tweet in tweetls:
+      #replace user profile if it doesn't exist
       tweet['user']['profile_image_url'] = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'
       tweet['user']['profile_image_url_https'] = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'  
       if (len(cachedOfficialTweets) > MAX_CACHE):
          cachedOfficialTweets.pop(0)
       cachedOfficialTweets.append(tweet)
 
+      #check if it needs to be filtered
       global searchval
       if not checksearch(tweet, searchval):
          return
@@ -126,6 +128,8 @@ def tweet(ctx, e):
 @event('chirpregular')
 def tweet(ctx, e):
    # we receive a tweet
+
+   #most of this is the same as in chirpofficial, so for docs refer to there
    tweetls = e.data
 
    #I've been able to locate the user image urls, because I couldn't stand the 'image not found' icon on every tweet. -Douwe Osinga
@@ -144,11 +148,5 @@ def tweet(ctx, e):
          return
       
    emit('regular', tweetls)
-
-
-
-# define a normal Python function
-def clip(lower, value, upper):
-    return max(lower, min(value, upper))
 
    
--- WeatherEventGen.py
+++ WeatherEventGen.py
@@ -10,14 +10,17 @@ import TWeather
 import Classifier
 from eca import fire, get_context, context_switch, register_auxiliary, auxiliary
 
-def supertweetgen(data, timescale=1000, loop=True, begintime="Mon Nov 28 15:55:54 +0000 2011", classifier_threshold = 2):
-    
+def supertweetgen(datacpy, timescale=1000, loop=True, begintime="Mon Nov 28 15:55:54 +0000 2011", classifier_threshold = 2):
+    #our own event generator, 
+    # if tweets are before begintime, all of them will be sent at once
+    #we've also made changes to javascript lib functions to create more functionality
+    data = datacpy[:]
     officialcatchuptweets = []
     regularcatchuptweets = []
     last_time = datetime.strptime(begintime, '%a %b %d %H:%M:%S %z %Y')
     for tweet in data:
         tweet_time = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
-        tweet['created_at'] = datetime.strftime(tweet_time, '%a %b %d %H:%M:%S %Y')
+        tweet['created_at'] = datetime.strftime(tweet_time, '%a %b %d %H:%M:%S %Y') #fix the date so timezone isn't shown
         if (tweet_time < last_time):
             #immediately print
             if Classifier.isofficial(tweet):
@@ -26,6 +29,7 @@ def supertweetgen(data, timescale=1000, loop=True, begintime="Mon Nov 28 15:55:5
                 if(Classifier.tweetIsAboutWeather_Certainty(tweet) > classifier_threshold):
                     regularcatchuptweets.append(tweet)
         else:
+            #send all tweets with fire_global
             if len(officialcatchuptweets) > 0:
                 fire_global("chirpofficial", officialcatchuptweets)
                 officialcatchuptweets = []
@@ -52,60 +56,6 @@ def supertweetgen(data, timescale=1000, loop=True, begintime="Mon Nov 28 15:55:5
         fire_global("chirpregular", regularcatchuptweets)
         regularcatchuptweets = []
     if (loop):
-        supertweetgen(data, timescale=1000, loop=True, begintime=begintime, classifier_threshold = 2)
+        supertweetgen(datacpy, timescale=1000, loop=True, begintime=begintime, classifier_threshold = 2)
         #repeat it
 
-
-
-def custom_tweet_gen(stop, data, time_factor=1000, dateconstraint=False):
-    """
-    Offline tweet replay.
-
-    Takes a datafile formatted with 1 tweet per line, and generates a sequence of
-    scaled realtime items.
-    """
-    # timing functions return false if we need to abort
-    def delayer():
-        return not stop.wait(delay / time_factor)
-
-    def immediate():
-        return not stop.is_set()
-
-    # select timing function based on time_factor
-    delayed = immediate if time_factor is None else delayer
-
-
-    
-    last_time = datetime.strptime("Mon Oct 30 15:55:54 +0000 2011", '%a %b %d %H:%M:%S %z %Y')
-    lines = 0
-    for tweet in data:
-        lines += 1
-
-        # time scale the tweet
-        tweet_time = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
-        tweetdate = tweet_time.date()
-        if ((tweetdate.day is not datetime.today().day) or (tweetdate.month is not datetime.today().month)) and dateconstraint:
-            continue
-        if (tweet_time < last_time):
-            continue
-
-        if not last_time:
-            last_time = tweet_time
-
-        wait = tweet_time - last_time 
-        delay = wait.total_seconds()
-
-        # delay and yield or break depending on success
-        if delayed():
-            yield tweet
-            last_time = tweet_time
-        else:
-            break
-
-
-def start_tweets(data, event_name='tweet', aux_name='tweeter', **kwargs):
-    context = get_context()
-    if context is None:
-        raise NotImplementedError("Can not start offline tweet replay outside of a context.")
-    register_auxiliary(aux_name, EventGenerator(context, generator=custom_tweet_gen, data=data, event_name=event_name, **kwargs))
-    auxiliary(aux_name).start()
